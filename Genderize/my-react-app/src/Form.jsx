3745de2b1c52d87dc2c730413b8b4781726676ae
griffela <100675308+Nasteeena@users.noreply.github.com>
2023-02-25 16:46:24 +0300

    state

---
+++ Genderize/my-react-app/src/Form.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TextInput from './TextInput'
+import Button from './Button'
+
+export default function Form() {
+    return (
+        <form>
+            <TextInput />
+            <Button />
+            <div>Result:</div>
+        </form>
+    )
+}
+
+
+
+
