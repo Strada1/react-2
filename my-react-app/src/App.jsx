dec96cb3116a335f633b6364dfca2957f8e8d908
Konstantin Kust <konstantinkust@gmail.com>
2023-03-02 19:05:53 +0300

    #8 Подъем состояния
    done: #8 Подъем состояния

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import TextInput from './Input';
 import Button from './Button';
 
 class Main extends React.Component {
@@ -7,54 +8,48 @@ class Main extends React.Component {
         super(props);
         this.state = {
             value: '',
-            nameLength: null,
             error: null,
             isLoaded: false,
             data: [],
         };
 
-        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(event) {
+    handleChange(value) {
         this.setState({
-            value: event.target.value,
-            nameLength: event.target.value.length,
+            value: value,
         });
-        console.log(event);
     }
 
     async handleSubmit(event) {
         event.preventDefault();
 
-        console.log(this.state.isLoaded);
-
+        const invalidСharacters = 2;
         const firstName = this.state.value;
+        const nameLength = firstName.length;
+
         const serverUrl = 'https://api.genderize.io';
         const url = `${serverUrl}?name=${firstName}`;
-        const invalidСharacters = 2;
 
         try {
             const response = await fetch(url);
             const data = await response.json();
-            if (this.state.nameLength <= invalidСharacters) {
-                this.setState({
-                    isLoaded: true,
-                });
+
+            if (nameLength <= invalidСharacters) {
                 throw new SyntaxError('Данные некорректны');
             }
             if (response.ok) {
                 if (!data.gender) {
                     throw new SyntaxError('Введите имя латиницей ');
                 }
+
                 this.setState({
                     isLoaded: true,
                     data: data,
                     error: null,
                 });
-
-                console.log(this.state.data);
             }
         } catch (error) {
             this.setState({
@@ -64,36 +59,27 @@ class Main extends React.Component {
         }
     }
 
-    handleCallback = (childData) => {
-        this.setState({ name: childData });
-    };
-
     render() {
         const isLoaded = this.state.isLoaded;
         const error = this.state.error;
         const gender = this.state.data.gender;
+        const name = this.state.data.name;
 
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <input
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                        className="inptText"
-                        type="text"
-                        placeholder="Введите имя"
-                    />
+                    <TextInput onValueChange={this.handleChange} />
                     <Button />
                 </form>
 
                 {isLoaded && error === null && gender && (
                     <div>
-                        {this.state.data.name} is {this.state.data.gender}
+                        {name} is {gender}
                     </div>
                 )}
 
                 {isLoaded && error !== null && (
-                    <div style={{ color: 'red' }}>{this.state.error}</div>
+                    <div style={{ color: 'red' }}>{error}</div>
                 )}
             </div>
         );
