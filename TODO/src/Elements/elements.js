20109a43a78e80b3cb562c10f0aa487ddec937e8
griffela <100675308+Nasteeena@users.noreply.github.com>
2023-03-10 19:29:26 +0300

    deleted smth

--- TODO/src/Elements/elements.js
+++ TODO/src/Elements/elements.js
@@ -8,16 +8,5 @@ const PRIORITY = {
     LOW: 'Low',
 };
 
-const ERRORS = {
-    errorAdd: 'You have not added any task in your list!',
-};
 
-const list = [ 
-    { name: 'Create a post', status: STATUS.IN_PROGRESS, priority: PRIORITY.LOW  }, 
-    { name: 'Make the bed', status: STATUS.TO_DO, priority: PRIORITY.LOW  },
-    { name: 'Do homework', status: STATUS.IN_PROGRESS, priority: PRIORITY.HIGH  }, 
-    { name: 'Pet the cat', status: STATUS.DONE, priority: PRIORITY.HIGH  }, 
-    { name: 'Play the computer', status: STATUS.DONE, priority: PRIORITY.LOW  },  
-];
-
-export {STATUS, list, ERRORS, PRIORITY};
+export {STATUS, PRIORITY};
